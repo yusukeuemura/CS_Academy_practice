f164be9e9355dce7b5f4f68002da6fd72d0152fd
simba <simba@inl.ics.keio.ac.jp>
2018-04-07 14:37:42 +0900

    path function was implemented

--- Graphs/BFS/matrix-exploration.cpp
+++ Graphs/BFS/matrix-exploration.cpp
@@ -1,2 +1,35 @@
 //matrix-exploration
 
+#include <matrix-exploration.h>
+#include <iostream>
+#define Q 64
+
+matrix-exploration::matrix-exploration()
+{
+}
+
+int matrix-exploration::top(Path *p)
+{
+        return p->path[p->len - 1];
+}
+
+bool matrix-exploration::visited(Path *p, int x)
+{
+        for (int i = 0; i < p->len; i++) {
+                if (p->path[i] == x) {
+                        return true;
+                }
+        }
+        return false;
+}
+
+void print_path(Path *p)
+{
+        for (int i = 0; i < p->len; i++) {
+                std::cout << p->path[i] - 1 << endl;
+        }
+}
+
+matrix-exploration::~matrix-exploration()
+{
+}
--- Graphs/BFS/matrix-exploration.h
+++ Graphs/BFS/matrix-exploration.h
@@ -1,2 +1,17 @@
 //matrix-exploration.h
 
+typedef struct{
+        int path[N];
+        int len;
+}Path;
+
+class matrix-exploration
+{
+public:
+        matrix-exploration();
+        ~matrix-exploration();
+        int top(Path *p);
+        bool visited(Path *p, int x);
+        void print_path(Path *p);
+};
+        
