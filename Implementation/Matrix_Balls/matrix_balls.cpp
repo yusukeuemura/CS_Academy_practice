a5a663ae76bdcc1efa84e5cecaedc474d8d2d18e
simba <simba@inl.ics.keio.ac.jp>
2018-10-10 17:51:01 +0900

    Upload matrix_balls.cpp

--- Implementation/Matrix_Balls/matrix_balls.cpp
+++ Implementation/Matrix_Balls/matrix_balls.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <map>
-#include <chrono>
 
 int main() {
     const int MAX = 400000;
-    int n, m, a[502][502] = {0};
+    int n, m, a[502][502], ne[502 * 502], ans[502 * 502];
+    int dir[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
     std::cin >> n >> m;
-    std::map<int, int> ne, balls;
     for (int i = 0; i <= m + 1; i++) {
         a[0][i] = MAX;
         a[n + 1][i] = MAX;
@@ -15,36 +13,37 @@ int main() {
         a[i][0] = MAX;
         a[i][m + 1] = MAX;
     }
-    for (int i = 1; i<= n; i++) {
+    for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
             std::cin >> a[i][j];
         }
     }
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
-            int tmp = a[i][j];
-            ne[tmp] = tmp;
-            for (int k = i - 1; k <= i + 1; k++) {
-                for (int l = j - 1; l <= j + 1; l++) {
-                    if (ne[tmp] > a[k][l]) {
-                        ne[tmp] = a[k][l];
-                    }
+            int nei = i, nej = j;
+            for (int k = 0; k < 8; k++) {
+                int tx = i + dir[k][0], ty = j + dir[k][1];
+                if (a[nei][nej] > a[tx][ty]) {
+                    nei = tx;
+                    nej = ty;
                 }
             }
+            ne[i * m + j] = nei * m + nej;
         }
     }
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
-            int tmp = a[i][j];
-            while (ne[tmp] != tmp) {
-                tmp = ne[tmp];
+            int ansi = i, ansj = j;
+            int index = ansi * m + ansj;
+            while (index != ne[index]) {
+                index = ne[index];
             }
-            balls[tmp]++;
+            ans[index]++;
         }
     }
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
-            std::cout << balls[a[i][j]] << " ";
+            std::cout << ans[i * m + j] << " ";
         }
         std::cout << std::endl;
     }
