a7dbe072870a3370ed10cc6f9311b60c7c48d0e4
simba <simba@inl.ics.keio.ac.jp>
2018-11-07 12:25:54 +0900

    Upload printer.cpp

--- Implementation/Printer/printer.cpp
+++ Implementation/Printer/printer.cpp
@@ -1,4 +1,6 @@
-#include <iosteram>
+#include <iostream>
+
+const int MAXVALUE = 1e9;
 
 int main() {
     long long int cartridges[4], price[4], bundle;
@@ -12,18 +14,22 @@ int main() {
     long long int buy = 0;
     while (1) {
         long long int eachSum = 0LL;
+        long long int m = MAXVALUE + 1;
         for (int i = 0; i < 4; i++) {
             if (cartridges[i] > 0) {
                 eachSum += price[i];
+                if (cartridges[i] < m) {
+                    m = cartridges[i];
+                }
             }
         }
         if (eachSum < bundle) {
             break;
         }
-        buy += bundle;
+        buy += bundle * m;
         for (int i = 0; i < 4; i++) {
             if (cartridges[i] > 0) {
-                cartridges[i]--;
+                cartridges[i] -= m;
             }
         }
     }
